4243e67bf9f4b33fbefa622d2ff571a7d43e6091
JoaoVictorG <joaovictorgon36@gmail.com>
2023-03-26 14:01:26 -0300

    Listar Usuarios

---
+++ Roless/Roles/Controllers/ListUsuarios.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Roles.Controllers
+{
+    public class ListUsuarios : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [Authorize(Roles = "Admin")]
+        public IActionResult ListUsers()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+    }
+}
---
+++ Roless/Roles/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Roles.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult ListUsers()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+    }
+}
